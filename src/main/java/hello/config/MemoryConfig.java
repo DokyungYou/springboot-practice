a94ee0b0c36f4f50a0e61d0d9a184af9f70626ef
DokyungYou <willow74rr@gmail.com>
2024-09-12 21:20:24 +0900

    @Conditional - 다양한 기능

--- src/main/java/hello/config/MemoryConfig.java
+++ src/main/java/hello/config/MemoryConfig.java
@@ -3,11 +3,13 @@ package hello.config;
 import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
-@Conditional(MemoryCondition.class)
+//@Conditional(MemoryCondition.class)
+@ConditionalOnProperty(name = "memory", havingValue = "on") // 환경정보가 memory=on 이라는 조건에 맞으면 동작, @Conditional(MemoryCondition.class) 과 동일하게 동작
 @Configuration
 public class MemoryConfig {
 
